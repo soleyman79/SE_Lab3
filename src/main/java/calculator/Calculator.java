5269b999fdec83b275be489d238a841799d131bd
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-08-04 14:17:01 +0330

    Complete calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,20 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public double divide(int a, int b) {
+        return (double) a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int power(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result = this.multiply(result, a);
+        }
+        return result;
+    }
 }
